bc4589742dd598e7a8b82dd7cd80151d7dd9a36a
neville wekesa <nevillewekesa2022@gmail.com>
2025-07-29 12:29:51 +0300

    Add files via upload
    #refreshment at PLP(power learn project)

---
+++ index.py
@@ -0,0 +1,10 @@
+#variables are container that stores data
+#Assign variables
+#displays the content using print function
+name = 'Neville'
+
+
+age = 30
+
+print("My name is ", name)
+print('I think this is my age,', 30)
